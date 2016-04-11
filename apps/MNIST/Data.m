a85dc5765cd59a232b2504f7205f7c261f381c72
jfrancis71 <julian.w.francis@gmail.com>
2016-04-11 10:01:45 +0100

    Removing Validation dataa
    Validation set should be decided by training regime, not by the data
    loader.

--- apps/MNIST/Data.m
+++ apps/MNIST/Data.m
@@ -22,23 +22,18 @@ ReadImagesFromMNISTFile[file_String] := Module[{
    Map[Reverse,Map[If[#1 > 128, 1., 0.] &,Map[Partition[#,28]&,RawImages],{3}]]
 ];
 
-ReadMINSTLabelFile[file_String] := Module[{
-   RawClassificationFileData},
-
+ReadMINSTLabelFile[file_String] := Module[
+   {RawClassificationFileData},
    RawClassificationFileData=BinaryReadList[file,"Byte",200000000];
-
    RawClassificationFileData[[9;;-1]]
 ];
 
-TrainingImages = ReadImagesFromMNISTFile[$CNDataDir<>"\\MNIST\\train-images-idx3-ubyte"][[1;;59500]];
-ValidationImages = ReadImagesFromMNISTFile[$CNDataDir<>"\\MNIST\\train-images-idx3-ubyte"][[59501;;60000]];
+TrainingImages = ReadImagesFromMNISTFile[$CNDataDir<>"\\MNIST\\train-images-idx3-ubyte"];
 TestImages = ReadImagesFromMNISTFile[$CNDataDir<>"\\MNIST\\t10k-images-idx3-ubyte"];
 
-TrainingLabels = ReadMINSTLabelFile[$CNDataDir<>"\\MNIST\\train-labels-idx1-ubyte"][[1;;59500]];
-ValidationLabels = ReadMINSTLabelFile[$CNDataDir<>"\\MNIST\\train-labels-idx1-ubyte"][[59501;;60000]];
+TrainingLabels = ReadMINSTLabelFile[$CNDataDir<>"\\MNIST\\train-labels-idx1-ubyte"];
 TestLabels = ReadMINSTLabelFile[$CNDataDir<>"\\MNIST\\t10k-labels-idx1-ubyte"];
 
 (* Converting to the 1 of K target format *)
-TrainingTargets = Map[ReplacePart[ConstantArray[0,10],(#+1)->1]&,TrainingLabels];
-ValidationTargets = Map[ReplacePart[ConstantArray[0,10],(#+1)->1]&,ValidationLabels];
-TestTargets = Map[ReplacePart[ConstantArray[0,10],(#+1)->1]&,TestLabels];
+TrainingTargets = Map[ReplacePart[ConstantArray[0.,10],(#+1)->1.]&,TrainingLabels];
+TestTargets = Map[ReplacePart[ConstantArray[0.,10],(#+1)->1.]&,TestLabels];
