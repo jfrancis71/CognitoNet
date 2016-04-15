84854fb83cb4b514c1ca91fa900712d4f7039f63
jfrancis71 <julian.w.francis@gmail.com>
2016-04-15 10:33:16 +0100

    Adding more utility functions

--- lib/CNUtils.m
+++ lib/CNUtils.m
@@ -22,6 +22,29 @@ Color inputs of neural nets are typically non interleaved and in seperate plane 
 CNColImage[image_]:=Image[image,Interleaving->False]
 
 
+CNImageRescale::usage = "CNImageRescale[image_,scale_] rescales image by scale factor.";
+CNImageRescale[image_,scale_] :=
+   ImageData[ImageResize[Image[image],scale*Length[image[[1]]]]]
+
+
+CNRescaleGraphics::usage = "CNRescaleGraphics[f,image,scale] will rescale image and pass it into function f
+which is expected to produce a list of Graphics objects and then rescale back this output.";
+CNRescaleGraphics[f_Function,image_?MatrixQ,scale_?NumberQ] :=
+   Graphics[Map[Scale[#[[1]],1/scale,{0,0}]&,f[CNImageRescale[image,scale]]]];
+
+
+CNBoundingRectangles::usage = "CNBoundingRectangles[coords, filterSize] draws a 
+rectangle of dimensions specified by filterSize {width,height} centered on each point in coords where
+points are specified by (y,x) with y in raster coordinates.";
+CNBoundingRectangles[coords_?MatrixQ,filterSize_?VectorQ]:=
+   Map[Rectangle[#-filterSize,#+filterSize]&,coords]
+
+
+CNOutlineGraphics::usage = "CNOutlineGraphics[grObjects,color] produces a Graphics object with the
+grObjects made transparent except for their edges.";
+CNOutlineGraphics[grObjects_,Color_:Green]:=Graphics[{Opacity[0],Color,EdgeForm[Directive[Color,Thick]],grObjects}]
+
+
 CNCameraMainLoop::usage = "CNCameraMainLoop[programF,width] evaluates and displays programF in an infinite loop.
 programF should be a function accepting a single argument which is the camera image in CNImage format.
 You should specify the width of the image that programF is expecting.";
