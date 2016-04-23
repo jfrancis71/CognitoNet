631fe9d816db0c5ab0a7e5cf8e8f45b328154808
jfrancis71 <julian.w.francis@gmail.com>
2016-04-23 09:08:49 +0100

    Tidying up image imports

--- lib/CNUtils.m
+++ lib/CNUtils.m
@@ -6,36 +6,46 @@ CNAssertAbort[bool_,message_]:=
       Print[message];Abort[]];
 
 
+(*
+Deprecated
 CNImage::usage = "CNImage[matrix] assumes matrix represents an image structure and displays it.
 The matrix is assumed to be in graphics order, i.e. row 1 of the matrix is the bottom of the image.";
 CNImage[matrix_?MatrixQ]:=Graphics[Raster[matrix]];
+*)
 
 
 CNImportImage::usage = "CNImport[image,width] takes a Mathematica Image object
-and returns it in CNImage format (ie Raster row ordering) of specified image width.
+and returns a grayscale image.
 CNImportImage[image,{width,height}] takes a Mathematica Image object
-and returns it in CNImage format (ie Raster row ordering) of specified image width and height.
-CNImportImage[file,width] reads an image file in and returns it in CNImage format (ie Raster row ordering) of specified image width.
-CNImportImage[file,{width,height}] reads an image file in and returns it in CNImage format (ie Raster row ordering) of specified image width.
+and returns a grayscale image of specified image width and height.
+CNImportImage[file,width] reads an image file in and returns a grayscale image of specified image width.
+CNImportImage[file,{width,height}] reads an image file in and returns a grayscale image of specified image width and height.
+This function provides an easy way to both convert to grayscale and resize at the same time.
 ";
 CNImportImage[image_Image,width_Integer]:=
-   ImageData[ColorConvert[ImageResize[image,width],"GrayScale"]]//Reverse
+   ColorConvert[ImageResize[image,width],"GrayScale"]
 CNImportImage[image_Image,{width_Integer,height_Integer}]:=
-   ImageData[ColorConvert[ImageResize[image,{width,height}],"GrayScale"]]//Reverse
+   ColorConvert[ImageResize[image,{width,height}],"GrayScale"]
 CNImportImage[file_String,width_Integer]:=
    CNImportImage[Import[file],width];
 CNImportImage[file_String,{width_Integer,height_Integer}]:=
    CNImportImage[Import[file],{width,height}];
 
 
+(*
+Deprecated
 CNColImage::usage = "CNColImage[data] will display image corresponding to data.
 Color inputs of neural nets are typically non interleaved and in seperate plane format, e.g. 3*32*32.";
 CNColImage[image_]:=Image[image,Interleaving->False]
+*)
 
 
+(*
+Deprecated
 CNImageRescale::usage = "CNImageRescale[image_,scale_] rescales image by scale factor.";
 CNImageRescale[image_,scale_] :=
    ImageData[ImageResize[Image[image],scale*Length[image[[1]]]]]
+*)
 
 
 CNRescaleGraphics::usage = "CNRescaleGraphics[f,image,scale] will rescale image and pass it into function f
@@ -57,7 +67,7 @@ CNOutlineGraphics[grObjects_,Color_:Green]:=Graphics[{Opacity[0],Color,EdgeForm[
 
 
 CNCameraMainLoop::usage = "CNCameraMainLoop[programF,width] evaluates and displays programF in an infinite loop.
-programF should be a function accepting a single argument which is the camera image in CNImage format.
+programF should be a function accepting a single argument which is the camera image.
 You should specify the width of the image that programF is expecting.";
 CNCameraMainLoop[programF_,width_] := 
    Module[{grOutput},Monitor[While[True,grOutput=programF[CNImportImage[CurrentImage[],width]]],grOutput]];
