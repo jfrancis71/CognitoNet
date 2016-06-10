f194ff4136d99d5a3d7bc6ba82c5c5bc0cafe72e
jfrancis71 <julian.w.francis@gmail.com>
2016-06-10 16:08:55 +0100

    initial revision

---
+++ apps/NoEntry/Training.m
@@ -0,0 +1,68 @@
+(* ::Package:: *)
+
+<<CNNeuralCore.m
+
+
+(*
+Set noEntryBaseDir and checkpointDir to relevent directories, eg
+
+   noEntryBaseDir = "C:\\Users\\julian\\ImageDataSets\\NoEntrySigns5";
+   checkpointDir = "NoEntry5\\History1\\NoEntryNet";
+*)
+
+
+trainingPositives=Map[
+   Function[fileName,
+      Map[ImageTake[#,{71-32,71+31},{137-32,137+31}]&,CNImportMovie[fileName,256]]],
+   FileNames[noEntryBaseDir<>"\\Training\\Positives\\*"]];
+
+
+validationPositives=Map[
+   Function[fileName,
+      Map[ImageTake[#,{71-32,71+31},{137-32,137+31}]&,CNImportMovie[fileName,256]]],
+   FileNames[noEntryBaseDir<>"\\Validation\\Positives\\*"]];
+
+
+trainingNegatives=Map[
+   Function[fileName,
+      Map[ImagePartition[#,{64,64}]&,CNImportMovie[fileName,256]]],
+   FileNames[noEntryBaseDir<>"\\Training\\Negatives\\*"]];
+
+
+validationNegatives=Map[
+   Function[fileName,
+      Map[ImagePartition[#,{64,64}]&,CNImportMovie[fileName,256]]],
+   FileNames[noEntryBaseDir<>"\\Validation\\Negatives\\*"]];
+
+
+trainingSet=RandomSample[Join[
+   Map[#->1&,Flatten[trainingPositives]],
+   Map[#->0&,Flatten[trainingNegatives][[1;;Length[Flatten[trainingPositives]]]]]
+]];
+
+
+validationSet=RandomSample[Join[
+   Map[#->1&,Flatten[validationPositives]],
+   Map[#->0&,Flatten[validationNegatives][[1;;Length[Flatten[validationPositives]]]]]
+]];
+
+
+SeedRandom[1234];
+NoEntryNet={
+   Convolve2DToFilterBankInit[16,5],Tanh,
+   MaxConvolveFilterBankToFilterBank,SubsampleFilterBankToFilterBank,ConvolveFilterBankToFilterBankInit[16,16,5],Tanh,
+   MaxConvolveFilterBankToFilterBank,SubsampleFilterBankToFilterBank,ConvolveFilterBankToFilterBankInit[16,16,5],Tanh,
+   ConvolveFilterBankTo2DInit[16,1],
+   Adaptor2DTo1D[9],
+   LogSumExp,
+   Logistic
+};
+
+
+(*{trainingSet,validationSet}=Import["C:\\Users\\julian\\ImageDataSets\\NoEntrySigns4\\DataSet.mx"];*)
+
+
+CNMiniBatchTrainModel[NoEntryNet,trainingSet,CNCrossEntropyLoss,{
+   ValidationSet->validationSet,Momentum->0.9,MomentumType->"Nesterov",
+   LearningRate->.001,
+   EpochMonitor:>CNCheckpoint[checkpointDir]}]
