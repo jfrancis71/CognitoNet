c1a6bf2d56b004a12685c0b6fe3fad595cf08bb7
jfrancis71 <julian.w.francis@gmail.com>
2016-10-31 07:57:28 +0000

    bringing in line with TensorFlow example

--- apps/MNIST/TensorFlow.m
+++ apps/MNIST/TensorFlow.m
@@ -3,7 +3,7 @@
 <<CNNeuralCore.m
 
 
-Set::write
+<<MNIST\\Data.m
 
 
 inp=Import["/Users/julian/tensorflow/lib/python2.7/site-packages/tensorflow/models/image/mnist/input.json"];
@@ -43,17 +43,44 @@ Softmax
 
 
 net={
+   PadFilter[2],
+   Convolve2DToFilterBankInit[32,5],
+   ReLU,
+   MaxPoolingFilterBankToFilterBank,
+   PadFilterBank[2],
+   ConvolveFilterBankToFilterBankInit[32,64,5],
+   ReLU,
+   MaxPoolingFilterBankToFilterBank,
+   AdaptorFilterBankTo1D[ 64,7,7],
+   FullyConnected1DTo1DInit[64*7*7,1024],
+   ReLU,
+   FullyConnected1DTo1DInit[1024, 10],
+   Softmax
+};
+
+
+ReLUNet={
 PadFilter[2],
-Convolve2DToFilterBankInit[32,5],
+Convolve2DToFilterBank[
+Table[Convolve2D[.1,.1*Table[Random[]-.5,{5},{5}]],{f,1,32}]
+],
 ReLU,
 MaxPoolingFilterBankToFilterBank,
 PadFilterBank[2],
-ConvolveFilterBankToFilterBankInit[64,32,5],
+ConvolveFilterBankToFilterBank[Table[ConvolveFilterBankTo2D[
+.1,.1*Table[Table[Random[]-.5,{5},{5}],{f,1,32}]]
+,{o,1,64}]],
 ReLU,
 MaxPoolingFilterBankToFilterBank,
 AdaptorFilterBankTo1D[ 64,7,7],
-FullyConnected1DTo1DInit[64*7*7,1024],
+FullyConnected1DTo1D[Table[.1,{1024}], .1*Table[Random[]-.5,{1024},{3136}]],
 ReLU,
-FullyConnected1DTo1DInit[1024, 10],
+FullyConnected1DTo1D[Table[.1,{10}], .1*Table[Random[]-.5,{10},{1024}]],
 Softmax
 };
+
+
+CNMiniBatchTrainModel[ CNConvertCPUToGPU[ReLU], TrainingSet[[1;;55000]],CNCrossEntropyLoss,{
+   ValidationSet->TrainingSet[[55001;;60000]],Momentum->0.9,MomentumType->"Nesterov",
+   LearningRate->.01,
+   EpochMonitor:>CNCheckpoint["DogNet\\History1"]}]
